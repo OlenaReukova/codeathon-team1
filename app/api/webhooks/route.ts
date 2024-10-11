b6b60ea2ae339417783a39a33b2bb7a3ffbde26b
Olena Reukova <kronverk01@gmail.com>
2024-10-11 08:39:41 +0100

    refactor:webhooks/user/route.ts

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,45 +1,36 @@
-import Stripe from "stripe";
-import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
+import { db } from '@/lib/db';
 
-import { stripe } from "@/lib/stripe";
-import { db } from "@/lib/db";
-import { sign } from "crypto";
+export async function POST(req: NextRequest) {
+  const body = await req.json();
 
-export async function POST(req: Request) {
-  const body = await req.text();
-  const signature = headers().get("Stripe-Signature") as string;
+  const existingCompany = await db.company.findFirst({
+    where: {
+      OR: [
+        { companyName: body.companyPersonName },
+        { contactEmail: body.contactPersonEmail },
+      ],
+    },
+  });
 
-  let event: Stripe.Event;
-
-  try {
-    event = stripe.webhooks.constructEvent(
-      body,
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET!
-    )
-  } catch (error: any) {
-    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
+  if (existingCompany) {
+    return NextResponse.json(
+      { message: 'Company is already registered or pending verification' },
+      { status: 400 }
+    );
   }
 
-  const session = event.data.object as Stripe.Checkout.Session;
-  const userId = session?.metadata?.userId;
-  const courseId = session?.metadata?.courseId;
+  await db.company.create({
+    data: {
+      companyName: body.companyName,
+      companyAddress: body.companyAddress,
+      contactPerson: body.contactPersonName,
+      contactEmail: body.contactPersonEmail,
+    },
+  });
 
-  if (event.type === "checkout.session.completed") {
-    if (!userId || !courseId) {
-      return new NextResponse(`Webhook Error: Missing metadata`, { status: 400 });
-    }
-
-    await db.purchase.create({
-      data: {
-        courseId: courseId,
-        userId: userId,
-      }
-    });
-  } else {
-    return new NextResponse(`Webhook Error: Unhandled event type ${event.type}`, { status: 200 })
-  }
-
-  return new NextResponse(null, { status: 200 });
+  return NextResponse.json(
+    { message: 'Registration successful' },
+    { status: 200 }
+  );
 }
