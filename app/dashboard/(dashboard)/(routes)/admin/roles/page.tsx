3c35d60243961dfb120b92568a8feedb55e4b205
Olena Reukova <kronverk01@gmail.com>
2024-10-13 21:12:37 +0100

    refactor:Page "app/dashboard/(dashboard)/(routes)/admin/roles/create/page.tsx"

--- app/dashboard/(dashboard)/(routes)/admin/roles/page.tsx
+++ app/dashboard/(dashboard)/(routes)/admin/roles/page.tsx
@@ -1,37 +1,33 @@
-'use client'
+// app/dashboard/(dashboard)/(routes)/admin/roles/page.tsx
+'use client';
 
-import { useOrganization } from '@clerk/nextjs'
-import { DataTable } from './_components/data-table'
-import { columns } from './_components/columns'
-import SelectRole from './_components/select-role'
+import { useOrganization } from '@clerk/nextjs';
+import { DataTable } from './_components/data-table';
+import { columns } from './_components/columns';
+import SelectRole from './_components/select-role';
 
-export const OrgMembersParams = {
-  memberships: {
-    pageSize: 5,
-    keepPreviousData: true,
-  },
-}
-
-// List of organization memberships. Administrators can
-// change member roles or remove members from the organization.
-export const ManageRoles = () => {
-  const { isLoaded, memberships } = useOrganization(OrgMembersParams)
+const ManageRoles = () => {
+  const { isLoaded, memberships } = useOrganization({
+    memberships: {
+      pageSize: 5,
+      keepPreviousData: true,
+    },
+  });
 
   const organizationUsers = memberships?.data?.map((mem) => {
-    return mem
-  })
+    return mem;
+  });
 
   if (!isLoaded) {
-    return <>Loading</>
+    return <>Loading</>;
   }
 
   return (
-    <div className="p-6">
-      <DataTable columns={columns} data={organizationUsers!}/>
+    <div className='p-6'>
+      <SelectRole />
+      <DataTable columns={columns} data={organizationUsers!} />
     </div>
-  )
-}
+  );
+};
 
 export default ManageRoles;
-
-<SelectRole/>
