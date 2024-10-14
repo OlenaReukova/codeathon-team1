63495a0ea27ad2f7de31a775e9724831d404b025
Olena Reukova <kronverk01@gmail.com>
2024-10-14 11:33:24 +0100

    fix:app/dashboard/(dashboard)/(routes)/search/page.tsx

--- app/dashboard/(dashboard)/(routes)/search/_components/category-item.tsx
+++ app/dashboard/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -1,19 +1,16 @@
-"use client";
+'use client';
 
-import qs from "query-string"; 
-import { IconType } from "react-icons";
+import qs from 'query-string';
+import { IconType } from 'react-icons';
 
-import { 
-  usePathname,
-  useRouter,
-  useSearchParams } from "next/navigation";
-import { cn } from "@/lib/utils";
-import { title } from "process";
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { title } from 'process';
 
 interface CategoryItemProps {
-  label: string,
+  label: string;
   value?: string;
-  icon?: IconType
+  icon?: IconType;
 }
 
 export const CategoryItem = ({
@@ -25,36 +22,36 @@ export const CategoryItem = ({
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const currentCategoryId = searchParams.get("categoryId");
-  const currentTitle = searchParams.get("title");
+  const currentCategoryId = searchParams.get('categoryId');
+  const currentTitle = searchParams.get('title');
 
   const isSelected = currentCategoryId === value;
 
   const onClick = () => {
-    const url = qs.stringifyUrl({
-      url: pathname,
-      query: {
-        title: currentTitle,
-        categoryId: isSelected ? null : value,
-      }
-    }, { skipNull: true, skipEmptyString: true });
- 
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          title: currentTitle,
+          categoryId: isSelected ? null : value,
+        },
+      },
+      { skipNull: true, skipEmptyString: true }
+    );
+
     router.push(url);
-  }
+  };
 
   return (
-    <button 
-    onClick={onClick}
-    className={cn(
-      "py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transistion",
-      isSelected && "border-sky-700 bg-sky-200/20 text-sky-800"
-    )}
-    type="button"
-    >
-      {Icon && <Icon size={20}/>}
-      <div className="truncate">
-        {label}
-      </div>
+    <button
+      onClick={onClick}
+      className={cn(
+        'py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transistion',
+        isSelected && 'border-sky-700 bg-sky-200/20 text-sky-800'
+      )}
+      type='button'>
+      {Icon && <Icon size={20} />}
+      <div className='truncate'>{label}</div>
     </button>
-  )
-}
+  );
+};
