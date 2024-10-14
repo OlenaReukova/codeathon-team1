cdea50750566ead24f9fae5d3406b86876c87af1
Olena Reukova <kronverk01@gmail.com>
2024-10-14 10:27:31 +0100

    fix: deleted member folder, changed organizations/route.ts

--- app/api/organizations/[organizationId]/invitations/[invitationId]/revoke/route.ts
+++ app/api/organizations/[organizationId]/invitations/[invitationId]/revoke/route.ts
@@ -1,25 +1,25 @@
-import { auth, clerkClient } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
+import { auth, clerkClient } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 export async function POST(
   req: Request,
-  { params }:{ params: { organizationId: string, invitationId: string } }
+  { params }: { params: { organizationId: string; invitationId: string } }
 ) {
   try {
     const { userId } = auth();
 
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse('Unauthorized', { status: 401 });
     }
     await clerkClient.organizations.revokeOrganizationInvitation({
       organizationId: params.organizationId,
       invitationId: params.invitationId,
       requestingUserId: userId,
-    })
-    
-    return NextResponse.json({ message: 'Invitation revoked' })
+    });
+
+    return NextResponse.json({ message: 'Invitation revoked' });
   } catch (error) {
-    console.log(error)
-    return NextResponse.json({ error: 'Error revoking invitation' })
+    console.log(error);
+    return NextResponse.json({ error: 'Error revoking invitation' });
   }
 }
--- app/api/organizations/[organizationId]/route.ts
+++ app/api/organizations/[organizationId]/route.ts
@@ -1,35 +1,34 @@
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
-import { db } from "@/lib/db";
+import { db } from '@/lib/db';
 
 export async function PATCH(
   req: Request,
-  { params }:{ params: { organizationId: string } }
+  { params }: { params: { organizationId: string } }
 ) {
-  try{
-    const {userId} = auth();
-    const{organizationId} = params;
+  try {
+    const { userId } = auth();
+    const { organizationId } = params;
     const values = await req.json();
 
-    if(!userId) {
-      return new NextResponse("unauthorized", {status: 401});
+    if (!userId) {
+      return new NextResponse('unauthorized', { status: 401 });
     }
 
     const organization = await db.organization.update({
       where: {
         id: organizationId,
-        userId
+        userId,
       },
       data: {
         ...values,
-      }
+      },
     });
 
     return NextResponse.json(organization);
-
-  } catch(error) {
-    console.log("[ORGANIZATION_ID", error);
-    return new NextResponse("Internal Error", {status: 500});
+  } catch (error) {
+    console.log('[ORGANIZATION_ID', error);
+    return new NextResponse('Internal Error', { status: 500 });
   }
 }
