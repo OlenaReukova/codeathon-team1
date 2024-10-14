b384d60b537371f3a2e8392bafdf0360f0379163
Olena Reukova <kronverk01@gmail.com>
2024-10-14 11:46:50 +0100

    fix:components/organization/create-organization.tsx

--- components/organization/create-organization.tsx
+++ components/organization/create-organization.tsx
@@ -1,27 +1,31 @@
-'use client'
+'use client';
 
-import { useOrganizationList } from '@clerk/nextjs'
-import { FormEventHandler, useState } from 'react'
+import { useOrganizationList } from '@clerk/nextjs';
+import { FormEventHandler, useState } from 'react';
 
 export default function CreateOrganization() {
-  const { createOrganization } = useOrganizationList()
-  const [organizationName, setOrganizationName] = useState('')
+  const { createOrganization } = useOrganizationList();
+  const [organizationName, setOrganizationName] = useState('');
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault()
-    createOrganization({ name: organizationName })
-    setOrganizationName('')
-  }
+    e.preventDefault();
+    if (createOrganization) {
+      createOrganization({ name: organizationName });
+      setOrganizationName('');
+    } else {
+      console.error('createOrganization is undefined');
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <input
-        type="text"
-        name="organizationName"
+        type='text'
+        name='organizationName'
         value={organizationName}
         onChange={(e) => setOrganizationName(e.currentTarget.value)}
       />
-      <button type="submit">Create organization</button>
+      <button type='submit'>Create organization</button>
     </form>
-  )
+  );
 }
