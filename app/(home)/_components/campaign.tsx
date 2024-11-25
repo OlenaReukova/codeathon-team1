6aac48f0f51b6353c8af95b605507e743e83fc64
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2024-11-25 10:08:36 +0000

    fix:pagination butons

--- app/(home)/_components/campaign.tsx
+++ app/(home)/_components/campaign.tsx
@@ -128,13 +128,13 @@ const Campaigns = ({ items }: CampaignsListProps) => {
       </div>
       <div className="p-6">
         <div className="flex flex-col md:flex-row md:justify-between mb-8 space-y-4 md:space-y-0 md:space-x-4">
-          <div className="relative flex-1 max-w-xs mx-auto">
+          <div className="relative flex-1 max-w-xs">
             <input
               type="text"
               placeholder="Search by title"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              className="border p-3 rounded-md w-full text-sm pl-12 focus:border-[#059669] focus:ring-[#059669] transition-all"
+              className="border p-3 rounded-md w-full text-sm pl-12 focus:border-[#37AB87] focus:ring-[#37AB87] transition-all"
             />
             <svg
               className="absolute left-3 top-3 h-5 w-5 text-gray-400"
@@ -187,8 +187,23 @@ const Campaigns = ({ items }: CampaignsListProps) => {
               <Button
                 onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
                 disabled={currentPage === 1}
-                className="bg-[#059669] hover:bg-[#037f57] text-white text-sm py-2 px-5 rounded-full transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#037f57] focus:ring-opacity-50"
+                className="flex items-center bg-[none] hover:bg-[none] text-[#059669] hover:text-[#037f57] text-sm py-2 px-5 transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#037f57] focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
               >
+                <svg
+                  width="15"
+                  height="15"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="mr-2"
+                >
+                  <path
+                    d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
                 Previous
               </Button>
               <span className="text-sm text-gray-600">
@@ -199,9 +214,24 @@ const Campaigns = ({ items }: CampaignsListProps) => {
                   setCurrentPage((prev) => Math.min(prev + 1, totalPages))
                 }
                 disabled={currentPage === totalPages}
-                className="bg-[#059669] hover:bg-[#037f57] text-white text-sm py-2 px-5 rounded-full transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#037f57] focus:ring-opacity-50"
+                className="flex items-center bg-[none] hover:bg-[none] text-[#059669] hover:text-[#037f57] text-sm py-2 px-5 transition-colors duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#037f57] focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
               >
                 Next
+                <svg
+                  width="15"
+                  height="15"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ml-2"
+                >
+                  <path
+                    d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
               </Button>
             </div>
           </div>
