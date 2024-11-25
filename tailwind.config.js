24cb3f45a628c3af80a38f808868effcda826572
Olena Reukova <34659641+OlenaReukova@users.noreply.github.com>
2024-11-25 09:35:51 +0000

    fix:global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ import { withUt } from "uploadthing/tw";
 module.exports = withUt({
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
